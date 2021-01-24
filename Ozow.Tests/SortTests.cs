96764da6233b679481cb82426b7b431fd8315f25
Johan Potgieter <johan.potgieter@muchasphalt.com>
2021-01-24 18:05:49 +0200

    Add all functionalities and tests. Move to DI

--- Ozow.Tests/SortTests.cs
+++ Ozow.Tests/SortTests.cs
@@ -10,29 +10,15 @@ namespace Ozow.Test
     public class SortTests
     {
         [Theory]
-        [InlineData("test","test")]
-        public void Sort_AllUpperCaseToLowerCase_TestSuccess(string input, string expected)
+        [InlineData("Hello world", "dehllloorw")]
+        [InlineData("Contrary to popular belief, the pink unicorn flies east.", "aaabcceeeeeffhiiiiklllnnnnooooppprrrrssttttuuy")]
+        public void Sort_VariableTest_TestSuccess(string input, string expected)
         {
             var sysUnderTest = new SortService();
 
             var result = sysUnderTest.Sort(input);
 
             result.Should().Be(expected);
-        }
-
-        [Fact]
-        public void Sort_TestToLowerCasePrivateMethod_TestSuccess()
-        {
-            Type type = typeof(SortService);
-
-            var test = Activator.CreateInstance(type);
-
-            MethodInfo method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
-                .Where(x => x.Name == "ConvertToLower" && x.IsPrivate).First();
-
-            var result = (string)method.Invoke(test, new object[] { "HELLO" });
-
-            result.Should().Be("hello");
         }
 
         [Fact]
@@ -45,9 +31,31 @@ namespace Ozow.Test
             MethodInfo method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
                 .Where(x => x.Name == "LoadAlphabet" && x.IsPrivate).First();
 
-            var result = (string[])method.Invoke(test, null);
+            var result = (char[])method.Invoke(test, null);
 
-            result.Should().Contain(new string[] { "a", "b" });
+            result.Should().Contain(new char[] { 'a', 'b' });
+        }
+
+        [Theory]
+        [InlineData('a', 0)]
+        [InlineData('b', 1)]
+        [InlineData('c', 2)]
+        [InlineData('d', 3)]
+        [InlineData('e', 4)]
+        [InlineData('A', 0)]
+        [InlineData('!', -1)]
+        public void Sort_FindValueByIndexPrivateMethod_TestSuccess(char input, int expected)
+        {
+            Type type = typeof(SortService);
+
+            var test = Activator.CreateInstance(type);
+
+            MethodInfo method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Where(x => x.Name == "FindIndexByValue" && x.IsPrivate).First();
+
+            var result = (int)method.Invoke(test, new object[] { input });
+
+            result.Should().Be(expected);
         }
     }
 }
